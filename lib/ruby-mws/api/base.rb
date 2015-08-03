c24d02a515da3c9af9dbc5ee3ed95e76a193e4d8
Josh Shupack <ynaught@gmail.com>
2015-08-02 19:43:12 -0700

    disabled debug output

--- lib/ruby-mws/api/base.rb
+++ lib/ruby-mws/api/base.rb
@@ -7,7 +7,7 @@ module MWS
     class Base
       include HTTParty
       parser MWS::API::BinaryParser
-      debug_output $stderr  # only in development
+      # debug_output $stderr  # This was causing: TypeError "Can't dump IO"
       #format :xml
       headers "User-Agent"   => "ruby-mws/#{MWS::VERSION} (Language=Ruby/1.9.3-p0)"
       headers "Content-Type" => "text/xml"
